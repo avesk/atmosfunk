4eb1346d9355f36a9d78b4239071e4e27ead555e
avesk <averykushner@gmail.com>
2018-07-09 20:46:22 -0700

    commit

--- project/atmosfunk_frontend/src/components/Track.js
+++ project/atmosfunk_frontend/src/components/Track.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import Draggable from 'react-draggable';
 
 class Track extends Component {
 
@@ -12,14 +13,25 @@ class Track extends Component {
         const source = this.props.source;
         const title = this.props.title;
         return(
-            <div className="track">
-                <h3>{title}</h3>
-                <ReactAudioPlayer
-                    src={source}
-                    autoPlay
-                    controls
-                />
-            </div>
+            <Draggable
+                axis="x"
+                handle=".handle"
+                defaultPosition={{x: 0, y: 0}}
+                position={null}
+                grid={[25, 25]}
+                onStart={this.handleStart}
+                onDrag={this.handleDrag}
+                onStop={this.handleStop}
+            >
+                <div className="track">
+                    <h3>{title}</h3>
+                    <ReactAudioPlayer
+                        src={source}
+                        autoPlay
+                        controls
+                    />
+                </div>
+            </Draggable>
         );
     }
 
